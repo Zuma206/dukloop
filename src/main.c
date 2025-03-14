8af2ef8cd5238176c0e13ae181bf3f2718664c13
James <zuma@zuma.eu.org>
2025-03-14 18:35:42 +0000

    Added basic duktape implementation

--- src/main.c
+++ src/main.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 
 int main() {
-  printf("Hello, World!\n");
+  duk_context *ctx = duk_create_heap_default();
+  duk_eval_string(ctx, "2 * 8");
+  double result = duk_get_number(ctx, -1);
+  printf("Result: %f\n", result);
   return EXIT_SUCCESS;
 }
