bbe3a3617afc8c62ad6a5470bd8ae9dea1c21caf
James <zuma@zuma.eu.org>
2025-03-15 02:18:22 +0000

    Added comments to filestreams module

--- src/filestreams.c
+++ src/filestreams.c
@@ -2,8 +2,10 @@
 #include <duktape.h>
 #include <stdio.h>
 
+/* A hidden symbol for accessing the FILE pointer for a FileStream class */
 static const char *FILE_STREAM_PROP = DUK_HIDDEN_SYMBOL("file_stream");
 
+/* The FileStream class constructor function */
 static duk_ret_t file_stream(duk_context *ctx) {
   duk_push_this(ctx);
   duk_swap_top(ctx, -2);
@@ -11,6 +13,7 @@ static duk_ret_t file_stream(duk_context *ctx) {
   return 0;
 }
 
+/* A common C function for the FileStream print and println methods */
 static void file_stream_print_c(duk_context *ctx, char *end) {
   const char *text = duk_get_string(ctx, -1);
   duk_push_this(ctx);
@@ -19,16 +22,19 @@ static void file_stream_print_c(duk_context *ctx, char *end) {
   fprintf(file_stream, "%s%s", text, end);
 }
 
+/* The FileStream print method */
 static duk_ret_t file_stream_print(duk_context *ctx) {
   file_stream_print_c(ctx, "");
   return 0;
 }
 
+/* The FileStream println method */
 static duk_ret_t file_stream_println(duk_context *ctx) {
   file_stream_print_c(ctx, "\n");
   return 0;
 }
 
+/* Creates the global FileStream class */
 static void file_stream_init(duk_context *ctx) {
   duk_push_c_function(ctx, file_stream, 1);
   duk_push_object(ctx);
@@ -40,6 +46,7 @@ static void file_stream_init(duk_context *ctx) {
   duk_put_global_literal(ctx, "FileStream");
 }
 
+/* Creates a global instance of the FileStream class bound to a FILE pointer */
 static void file_stream_new_global(duk_context *ctx, char *name, FILE *file) {
   duk_get_global_literal(ctx, "FileStream");
   duk_push_pointer(ctx, file);
--- src/filestreams.h
+++ src/filestreams.h
@@ -3,6 +3,10 @@
 
 #include <duktape.h>
 
+/*
+  Initialise the full FileStream API on the global scope.
+  Includes FileStream, stdout, and stderr
+*/
 void dloop_file_stream_api_init(duk_context *ctx);
 
 #endif
